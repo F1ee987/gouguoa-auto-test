dc3204609a3ded6e9fc28fb626c42861ca93f9da
F1ee <2296543811@qq.com>
2026-08-14 13:52:15 +0800

    refactor: 移除旧的登录尝试逻辑并使用pytest

--- run.py
+++ run.py
@@ -5,27 +5,7 @@
 @Author :zhousha
 @Date   :2026/8/9 14:08
 """
-import requests
-from time import time
-import random
+import pytest
 
-session = requests.Session()
-possible_values = list(range(10, 40))
-random.shuffle(possible_values)
-i = []
-for answer in possible_values:
-    login_data = {
-        'username': "zhangsan",
-        'password': '123456',
-        'captcha': str(answer)
-    }
-    session.get(f"http://192.168.198.133:81/captcha.html?t={int(time()*1000)}")
-    res = session.post(url="http://192.168.198.129:81/home/login/login_submit", data=login_data)
-    print(res.json())
-    if "验证码不正确" in res.json().get('msg'):
-        print(f"当前答案>>{answer}")
-        i.append(answer)
-        continue
-    else:
-        break
-print(sorted(i))
+if __name__ == '__main__':
+    pytest.main()
