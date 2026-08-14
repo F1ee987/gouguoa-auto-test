809c31d87b3264cbb69e39b436e964af13d4e2dc
F1ee <2296543811@qq.com>
2026-08-14 13:51:14 +0800

    fix: 修正配置文件中的URL和数据库配置

--- config/conf.py
+++ config/conf.py
@@ -5,6 +5,7 @@
 @Author :zhousha
 @Date   :2026/7/23 13:27
 """
+#URL信息
 BASE_URL = "http://192.168.198.133:81"  # 基础URL
 LOGIN_URL = f"{BASE_URL}/home/login/login_submit"
 ADD_ACCOUNT_URL = f"{BASE_URL}/user/user/add_user"
@@ -12,6 +13,7 @@ LOGIN_TIP_LOC = ("XPASS", '//*[@id="layui-layer8"]')
 
 #数据库配置
 HOST = "192.168.198.133"
-PORT = "3306"
+PORT = 3306
 USERNAME = "root"
 PASSWORD = "root"
+DATABASE = "oa"
