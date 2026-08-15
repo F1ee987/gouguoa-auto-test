14980c6bec26b6702415cd638f701c6ba974adf7
F1ee <2296543811@qq.com>
2026-08-15 15:59:21 +0800

    feat: 增加个人编辑页面URL

--- config/conf.py
+++ config/conf.py
@@ -12,6 +12,7 @@ BASE_URL = "http://192.168.198.133:81"  # 基础URL
 LOGIN_URL = f"{BASE_URL}/home/login/login_submit"
 ADD_ACCOUNT_URL = f"{BASE_URL}/user/user/add"
 LOGIN_TIP_LOC = ("XPASS", '//*[@id="layui-layer8"]')
+EDIT_PERSONAL = f"{BASE_URL}/home/index/edit_personal"
 
 #数据库配置
 DB = {
