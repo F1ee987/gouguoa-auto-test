49cbf8ba8b907d70ff71d3baaaedc1de3e9f5ae1
F1ee <2296543811@qq.com>
2026-08-19 15:42:23 +0800

    feat: 新增测试用例运行功能

--- run.py
+++ run.py
@@ -7,5 +7,26 @@
 """
 import pytest
 
-if __name__ == '__main__':
+def only_run_api_test():
+    """
+    只运行api测试用例
+    :return:
+    """
+    pytest.main(["./api_test/test_cases"])
+
+def only_run_ui_test():
+    """
+    只运行ui测试用例
+    :return:
+    """
+    pytest.main(["./ui_test/test_cases"])
+
+def run_all_test():
+    """
+    运行所有测试用例
+    :return:
+    """
     pytest.main()
+
+if __name__ == '__main__':
+    only_run_ui_test()
